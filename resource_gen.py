912012a7e0ca0ba1efc87b5ff3b5ddbf1608c5a1
Mohammad Sahihi <msahihi1@gwdg.de>
2017-11-20 16:20:14 +0100

    updated

--- resource_gen.py
+++ resource_gen.py
@@ -2,22 +2,18 @@
 import logging
 import os
 from fabric.api import put, run as fabric_run, settings
-
 from HeatStack import HeatStack
 
 logger = logging.getLogger(__name__)
 
 
-def ssh_to(root_path, key_path, remote_server, **args):
+def ssh_to(root_path, private_key, remote_server, user, **args):
     logging.info("\n# Swarm Manager IP address is : " + remote_server)
-    with settings(host_string=remote_server,
-                  key_filename="/tmp/" + key_path +
-                  "/private.key", user="cloud"):
+    with settings(host_string=remote_server, key_filename=private_key, user=user):
         fabric_run(
             '[ -d ~/benchmark/cs-datacaching ] || mkdir -p ~/benchmark/cs-datacaching')
         put(root_path + '/asset/', '~/benchmark/cs-datacaching/')
-        put(root_path + '/prepare.sh', '~/benchmark/cs-datacaching/')
-        fabric_run('sudo chmod 750 ~/benchmark/cs-datacaching/prepare.sh')
+        fabric_run('sudo chmod 750 ~/benchmark/cs-datacaching/asset/prepare.sh')
         fabric_run('echo $(hostname) >>  /tmp/exp.txt ')
         fabric_run('echo "[+] Installing SNAP ....."')
         fabric_run(
@@ -28,20 +24,15 @@ def ssh_to(root_path, key_path, remote_server, **args):
         fabric_run('sudo chmod 777 /var/log/benchmark/')
         fabric_run(
             'sudo echo -e "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0\n" >> /var/log/benchmark/detail.csv')
-        fabric_run('cd ~/benchmark/cs-datacaching/ && ls')
-        fabric_run("cd ~/benchmark/cs-datacaching/ && ./prepare.sh " +
+        fabric_run("cd ~/benchmark/cs-datacaching/asset && ./prepare.sh " +
                    args['server_no'] + " " +
                    args['server_threads'] + " " +
                    args['memory'] + " " +
                    args['object_size'])
 
-def experiment_path():
-    experiment_path = os.path.join(
-        os.path.join(os.getcwd(), os.pardir), os.pardir)
-    return experiment_path
 
 def get_heat_args(params):
-    stack_abs_path = os.path.join(experiment_path(), params['stack_template'])
+    stack_abs_path = os.path.join(os.path.dirname(__file__), params['stack_template'])
     heat_args = {
         'auth_url': params['OpenStack_auth_url'],
         'username': params['OpenStack_username'],
@@ -55,6 +46,7 @@ def get_heat_args(params):
     }
     return heat_args
 
+
 def get_benchmark_args(params):
     benchmark_args = {
         'server_no': params['server_no'],
@@ -64,6 +56,7 @@ def get_benchmark_args(params):
     }
     return benchmark_args
 
+
 def deploy(context):
     resource = context.v1.resource.config
     params = resource['params']
@@ -72,39 +65,29 @@ def deploy(context):
     stack = HeatStack(**heat_args)
     stack.create()
     ip = stack.get_manager_ip()
-    stack_config_path = experiment_path()
+    root_path = os.path.dirname(__file__)
     instances_names = stack.get_all_instances()
-    root_path = os.path.join(os.path.dirname(__file__))
+    endpoint = {
+        'swarm_manager': {'ip': ip,
+                          'user': 'cloud',
+                          'private_key': os.path.join('/tmp/', params['experiment_name'], 'private.key'),
+                          },
+        'instances': instances_names,
+
+    }
     ssh_to(
         root_path,
-        params['experiment_name'],
-        ip,
+        endpoint['swarm_manager']['private_key'],
+        endpoint['swarm_manager']['ip'],
+        endpoint['swarm_manager']['user'],
         **benchmark_args
     )
-    endpoint = {
-        'ip': ip,
-        'stack_config_path': stack_config_path,
-        'instances': instances_names
-    }
     return endpoint
 
 
 def clean(context):
     resource = context.v1.resource.config
     params = resource['params']
-    experiment_path = os.path.join(
-        os.path.join(os.getcwd(), os.pardir), os.pardir)
-    stack_abs_path = os.path.join(experiment_path, params['stack_path'])
-    heat_args = {
-        'auth_url': params['OpenStack_auth_url'],
-        'username': params['OpenStack_username'],
-        'password': params['OpenStack_password'],
-        'tenant_name': params['OpenStack_tenant_name'],
-        'project_name': params['OpenStack_project_name'],
-        'stack_name': params['experiment_name'],
-        'key_name': params['experiment_name'],
-        'number_of_node': params['stack_server_no'],
-        'template': stack_abs_path
-    }
+    heat_args = get_heat_args(params)
     stack = HeatStack(**heat_args)
     stack.delete()
